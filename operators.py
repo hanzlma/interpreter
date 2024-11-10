2c191245330dab953489967314f988d212da33dc
hanzlma <hanzlma@lab.uzlabina.cz>
2024-11-10 14:12:23 +0100

    Dynamic operation for logical operators

--- operators.py
+++ operators.py
@@ -1,3 +1,4 @@
+import re
 operators = {
     "plus" : '+',
     "minus": '-',
@@ -8,44 +9,16 @@ operators = {
 }
 
 def SplitByOperators(string: str) -> list[str]:
-    split1 = string.split(operators['plus'])
-    split2: list[str] = []
-    for x in split1:
-        split2.extend(x.split(operators['minus']))
-    split3: list[str] = []
-    for x in split2:
-        split3.extend(x.split(operators['multiply']))
-    split4: list[str] = []
-    for x in split3:
-        split4.extend(x.split(operators['divide']))
+    split1: list[str] = re.split('\+|-|\*|/|&&|\|\|', string)
     output: list[str] = []
-    for x in split4:
+    for x in split1:
         output.append(x.strip())
     return output
 
 def GetOperatorsFromText(string: str) -> list[str]:
     operatorOccurencies: dict[int, str] = {}
-    previousIndex: int = 0
-    for i in range(string.count(operators['plus'])):
-        index = string.index(operators['plus'], previousIndex)
-        operatorOccurencies[index] = operators['plus']
-        previousIndex = index + 1
-    previousIndex = 0
-    for i in range(string.count(operators['minus'])):
-        index = string.index(operators['minus'], previousIndex)
-        operatorOccurencies[index] = operators['minus']
-        previousIndex = index + 1
-    previousIndex = 0
-    for i in range(string.count(operators['multiply'])):
-        index = string.index(operators['multiply'], previousIndex)
-        operatorOccurencies[index] = operators['multiply']
-        previousIndex = index + 1
-    previousIndex = 0
-    for i in range(string.count(operators['divide'])):
-        index = string.index(operators['divide'], previousIndex)
-        operatorOccurencies[index] = operators['divide']
-        previousIndex = index + 1
-    previousIndex = 0
+    for operator in re.finditer('\+|-|\*|/|&&|\|\|', string):
+        operatorOccurencies[operator.start()] = operator[0]
     operatorOccurencies = {key: operatorOccurencies[key] for key in sorted(operatorOccurencies)}
     output: list[str] = []
     for occurency in list(operatorOccurencies.values()):
